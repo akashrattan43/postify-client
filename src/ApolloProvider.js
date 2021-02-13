685cf67717831a712a94ab35d269639c5d07993b
akash rattan <akashrattan43@Gmail.com>
2021-02-13 15:05:46 -0800

    edited proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://murmuring-island-82685.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
